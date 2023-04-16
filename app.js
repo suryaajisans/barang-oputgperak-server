3536b9abea6f7c024a7d0aba2ac6c2f84bc5c2e0
suryaajisan <suryaajisan@gmail.com>
2023-04-17 05:07:00 +0700

    server build

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 7777;
+const port = process.env.PORT || 3000;
 const router = require('./routes/index')
 
 app.use(cors());
